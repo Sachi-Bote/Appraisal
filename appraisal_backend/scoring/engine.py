6ee39708c1c10cd1fd5f1396c5e4b7e5b9de62fe
Vineet Y <102981677+Geek0805@users.noreply.github.com>
2025-11-30 09:53:04 +0530

    Refactor scoring functions for clarity and parameters

--- appraisal_backend/scoring/engine.py
+++ appraisal_backend/scoring/engine.py
@@ -1,67 +1,21 @@
-def teaching(classes_taught, total_classes):
-    """
-    Calculate the percentage of classes taught.
-    classes include all, lectures, practicals, lab sessions, tutorials etc
-    Args:
-        classes_taught (int): The number of classes taught.
-        total_classes (int): The total number of classes.
-
-    Returns:
-        str: The teaching performance category.
-    """
+def teaching(classes_taught, assigned_classes):
     if total_classes == 0:
         grade = 0
     else:
-        grade = (classes_taught / total_classes) * 100
+        grade = (classes_taught / assigned_classes) * 100
     
     if grade >= 80:
-        return 'good'
-    elif grade >= 70:
-        return 'satisfactory'
-    else:
-        return 'not satisfactory'
-
-def Activity_Score():
-    """Involvement in the
-university /college
-student related
-activities /research
-activities :
-(a) Administrative
-responsibilities
-such as head,
-chairperson/
-Dean
-/Director/ Coordinator,
-warden etc. (b) Examination and evaluation
-duties assigned by the
-college / university or
-attending the examination
-paper evaluation.
-(c) Student related co-curricular,
-extension and field based
-activities such as student
-clubs, career counseling,
-study visits, student
-seminars and other events,
-cultural, sports, NCC, NSS
-and community services.
-(d) Organization seminars /
-conference /workshop, other
-college /university activities.
-(e) Evidence of activity involved
-in guiding PhD students.
-(f) Conducting minor or major
-research project
-(g) Sponsored by national or
-international agencies.
-At least one single or joint
-publication in peer-reviewed or
-UGC list of journals. """
-
-    if no_of_activities >= 3:
         return 'Good'
-    elif 1 > no_of_activities < 2:
+    elif grade >= 70:
+        return 'Satisfactory'
+    else:
+        return 'Not Satisfactory'
+
+def Activity_Score(No_of_Activities):
+
+    if No_of_Activities >= 3:
+        return 'Good'
+    elif 1 > No_of_Activities < 2:
         return 'Satisfactory'
     else:
         return 'Not Satisfactory'
@@ -71,7 +25,8 @@ UGC list of journals. """
 
 #Now for Table 2
 
-def research_scoring(papers=0, intl_books=0, nat_books=0):
+def research_scoring(Research_papers, International_publisher, National_publisher, Chapter_in_Edited_Book, Editor_of_Book_by_International_Publisher, Editor_of_Book_by_National_Publisher, Translation_in_Chapter_or_Research_Paper, Translation_in_Book):   
+    score = 0        
     #if Published Research Papers in Peer-Reviewed or UGC listed Journals score = +8
     if Research_papers == 'Yes':
         score += 8
@@ -92,7 +47,7 @@ def research_scoring(papers=0, intl_books=0, nat_books=0):
 
     return score
 
-def research_guidance():
+def research_guidance(PhD_degree_awarded, PhD_thesis_submitted, MPhil_or_PG_dissertation_awarded, research_consultancy, research_project_completed, research_project_ongoing, amount):
     score = 0
     if PhD_degree_awarded== 'Yes':
         score += 10
@@ -115,7 +70,8 @@ def research_guidance():
         else:
             score += 2
 
-def  patents():
+def  patents( patents_filed, policy_document, Awards_or_fellowship, international_level, national_level, state_level):
+    score = 0
     if patents_filed == 'Yes':
         if international_level == 'Yes':
             score += 10
@@ -138,7 +94,8 @@ def  patents():
 
     return score
 
-def presented_papers():
+def presented_papers( presented, international_level_abroad, national_level, state_level, international_level_india):
+    score = 0
     if presented == 'Yes':
         if international_level_abroad == 'Yes':
             score += 7
@@ -150,7 +107,8 @@ def presented_papers():
             score += 5
     return score
 
-def pedagogy_creation():
+def pedagogy_creation( developed_innovative_pedagogy, designed_new_curriculum, MOOCs, complete_course_in_4_quadrants, content_writer, course_coordinator, per_lecture_moocs, e_content, editor_of_e_content, contributor, content_per_module):
+    score = 0
     if developed_innovative_pedagogy == 'Yes':
         score += 5
     if designed_new_curriculum == 'Yes':
