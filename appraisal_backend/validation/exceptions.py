024647049123fa04170449fcfad524e754e10fcc
Sachi Bote <sachibote@gmail.com>
2026-02-24 09:25:33 +0530

    Delete appraisal_backend directory

--- appraisal_backend/validation/exceptions.py
+++
@@ -1,6 +0,0 @@
-# validation/exceptions.py
-class ValidationError(Exception):
-    """Raised when validation fails with a message."""
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
